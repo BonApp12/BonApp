2f1c501bae6899801f8426c57c68aa2823e3a995
Yassine Bousaidi <Yassine.bousaidi@hotmail.com>
2022-02-19 22:13:37 +0100

    :white_check_mark: feature :white_check_mark:Ajout des fixtures pour restaurants.factory.ts et address.factory.ts

--- packages/backend/src/database/factories/restaurants.factory.ts
+++ packages/backend/src/database/factories/restaurants.factory.ts
@@ -1,20 +1,22 @@
 // user.factory.ts
 import Faker from 'faker'
-import {define} from 'typeorm-seeding';
+import {define, factory} from 'typeorm-seeding';
 import {Restaurant} from '../../restaurant/entities/restaurant.entity';
+import {Address} from '../../address/entities/address.entity';
 
 define(Restaurant, (faker: typeof Faker) => {
-    const firstName = faker.name.firstName()
+    const firstName = faker.name.firstName();
     const lastName = faker.name.lastName();
 
-    const restaurant = new Restaurant()
+    const restaurant = new Restaurant();
     restaurant.name = faker.company.companyName();
     restaurant.contact_firstname = `${firstName}`;
     restaurant.contact_lastname = `${lastName}`;
     restaurant.contact_email = (`${firstName}.${lastName}@gmail.com`).toLowerCase();
     restaurant.siren = faker.finance.account();
-    restaurant.contact_phone = '0610203010';
-    return restaurant
-})
+    restaurant.contact_phone = faker.phone.phoneNumber('06########');
+    restaurant.address = factory(Address)() as any;
+    return restaurant;
+});
 
 
--- packages/backend/src/database/factories/users.factory.ts
+++ packages/backend/src/database/factories/users.factory.ts
@@ -6,17 +6,17 @@ import {UserRole} from '../../users/UserRole.enum';
 import {Restaurant} from '../../restaurant/entities/restaurant.entity';
 
 define(Users, (faker: typeof Faker) => {
-    const firstName = faker.name.firstName()
+    const firstName = faker.name.firstName();
     const lastName = faker.name.lastName();
 
-    const user = new Users()
-    user.firstname = `${firstName}`
-    user.lastname = `${lastName}`
-    user.email = (`${firstName}.${lastName}@gmail.com`).toLowerCase()
+    const user = new Users();
+    user.firstname = `${firstName}`;
+    user.lastname = `${lastName}`;
+    user.email = (`${firstName}.${lastName}@gmail.com`).toLowerCase();
     user.password = '123';
     user.role = UserRole[Object.keys(UserRole)[Math.floor(Math.random() * Object.keys(UserRole).length)]];
-    user.restaurant = factory(Restaurant)() as any
-    return user
-})
+    user.restaurant = factory(Restaurant)() as any;
+    return user;
+});
 
 
