3672be34ebf4f0ba9bd0f23a1fe63cd8934812b5
Yassine Bousaidi <Yassine.bousaidi@hotmail.com>
2022-02-19 04:49:24 +0100

    ✨feature✨ Ajout du chiffrement du mot de passe avant l'insertion ✨feature✨Ajout de commande pour les fixtures :memo: Ajout de documentation au readme.md

--- packages/backend/src/database/factories/users.factory.ts
+++ packages/backend/src/database/factories/users.factory.ts
@@ -12,9 +12,7 @@ define(Users, (faker: typeof Faker) => {
     user.firstname = `${firstName}`
     user.lastname = `${lastName}`
     user.email = (`${firstName}.${lastName}@gmail.com`).toLowerCase()
-    //mot de passe 'troisquatre'
-    user.password = '$2a$10$ZfOdPIp3yVD7fZR1m86Lv.k4sTTTy1PaWXV0yZ4jH./hexi7Bzm3W'
-    // génération aléatoire du role du user
+    user.password = '123';
     user.role = UserRole[Object.keys(UserRole)[Math.floor(Math.random() * Object.keys(UserRole).length)]];
     return user
 })
--- packages/backend/src/users/entities/users.entity.ts
+++ packages/backend/src/users/entities/users.entity.ts
@@ -1,52 +1,53 @@
-import {
-  BaseEntity,
-  Column,
-  Entity,
-  ManyToOne,
-  OneToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
-import { Restaurant } from '../../restaurant/entities/restaurant.entity';
-import { UserRole } from '../UserRole.enum';
-import { Exclude, instanceToPlain } from 'class-transformer';
-import { Order } from '../../orders/entities/order.entity';
+import {BaseEntity, BeforeInsert, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn,} from 'typeorm';
+import {Restaurant} from '../../restaurant/entities/restaurant.entity';
+import {UserRole} from '../UserRole.enum';
+import {Exclude, instanceToPlain} from 'class-transformer';
+import {Order} from '../../orders/entities/order.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Entity()
 export class Users extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column('varchar', { length: 150 })
-  firstname: string;
+    @Column('varchar', {length: 150})
+    firstname: string;
 
-  @Column('varchar', { length: 150 })
-  lastname: string;
+    @Column('varchar', {length: 150})
+    lastname: string;
 
-  @Column('varchar', { length: 150, unique: true })
-  email: string;
+    @Column('varchar', {length: 150, unique: true})
+    email: string;
 
-  @Exclude({ toPlainOnly: true })
-  @Column('varchar', { length: 255 })
-  password: string;
+    @Exclude({toPlainOnly: true})
+    @Column('varchar', {length: 255})
+    password: string;
 
-  @Column({
-    type: 'enum',
-    enum: UserRole,
-    default: UserRole.CLIENT,
-  })
-  role: UserRole;
+    @Column({
+        type: 'enum',
+        enum: UserRole,
+        default: UserRole.CLIENT,
+    })
+    role: UserRole;
 
-  @ManyToOne(() => Restaurant, (restaurant) => restaurant.users)
-  restaurant: Restaurant;
+    @ManyToOne(() => Restaurant, (restaurant) => restaurant.users)
+    restaurant: Restaurant;
 
-  @OneToMany(() => Order, (order) => order.user)
-  orders: Order[];
+    @OneToMany(() => Order, (order) => order.user)
+    orders: Order[];
 
-  @Column({ nullable: true })
-  @Exclude()
-  public currentHashedRefreshToken?: string;
+    @Column({nullable: true})
+    @Exclude()
+    public currentHashedRefreshToken?: string;
 
-  toJson() {
-    return instanceToPlain(this);
-  }
+    @BeforeInsert()
+    async setPassword(password: string) {
+        const salt = await bcrypt.genSalt()
+        this.password = await bcrypt.hash(password || this.password, salt)
+    }
+
+
+    toJson() {
+        return instanceToPlain(this);
+    }
 }
