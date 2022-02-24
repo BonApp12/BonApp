4ba8416a56c8edf89838654a5e19fe59318066a0
develop server <yassine.bousaidi@hotmail.com>
2022-02-24 20:41:10 +0000

    juste pour tester

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Deploying changes..."
+
+echo "Stop all container ❌"
+docker-compose stop
+
+echo "destroying last container instance ... say sayonara👋"
+# Suppression des instances
+docker system prune -a -f
+
+echo "executing git pull 🚣🏼‍"
+# Pull changes from the live branch
+git pull
+
+echo "rebuilding our life ..."
+
+docker-compose up  -d
+
+
