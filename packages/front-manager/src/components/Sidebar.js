b52bd253ff47c306cb70cd4d555461965b7d379a
BOUABDELLAH Marwane <manbou92@hotmail.fr>
2022-02-26 02:25:15 +0100

    Add logo BonApp to the dashboard

--- packages/front-manager/src/components/Sidebar.js
+++ packages/front-manager/src/components/Sidebar.js
@@ -3,7 +3,6 @@ import React from "react";
 function Sidebar() {
 
     const user = 'MBLH';
-    const title = 'Dashboard de ';
 
     const categorys = [
         {
@@ -29,18 +28,17 @@ function Sidebar() {
     ];
     return (
         <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-light">
-            <div
-                className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100">
-                <a href="/"
-                   className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-decoration-none">
-                    <span className="fs-5 d-none d-sm-inline">{title}{user}</span>
+            <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100">
+                <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-decoration-none">
+                    <img src="https://www.svgrepo.com/show/113600/restaurant.svg" alt="logo" className="logo"/>
+                    <span className="fs-5 d-none d-sm-inline text-dark m-3">Dashboard de {user}</span>
                 </a>
                 <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
                     id="menu">
                     {categorys.map((value, index) => {
                         return (
                             <li key={index}>
-                                <a href="#" className="nav-link align-middle px-0">
+                                <a href="/" className="nav-link align-middle px-0">
                                     <i className={value.icon}/>
                                     <span className="ms-1 d-none d-sm-inline p-2">
                                         {value.name}
