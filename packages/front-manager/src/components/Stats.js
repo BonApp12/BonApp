242d0087d01485a5fc5a6da5bee3808878d29321
BOUABDELLAH Marwane <manbou92@hotmail.fr>
2022-02-26 03:28:16 +0100

    [COMPONENT] Add Stats component

---
+++ packages/front-manager/src/components/Stats.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+function Stats() {
+
+    const stats = [
+        {
+            class: 'card border-3 border-end-0 border-primary mb-3',
+            title: 'Nombre de plats',
+            value: Math.random()
+        },
+        {
+            class: 'card border-3 border-start-0 border-end-0 border-info mb-3',
+            title: 'Plat le plus commandé',
+            value: 'Grec'
+        },
+        {
+            class: 'card border-3 border-start-0 border-end-0 border-warning mb-3',
+            title: 'Commandes',
+            value: Math.random()
+        },
+        {
+            class: 'card border-3 border-start-0 border-dark mb-3',
+            title: 'Utilisateurs',
+            value: Math.random()
+        }
+    ];
+
+    return (
+        <div className='p-3'>
+            <h3 className='p-3'>Statistiques principales</h3>
+            <div className="row">
+                {stats.map((stat) => {
+                    return (
+                        <div className="col-sm-3 text-center">
+                            <div className={stat.class}>
+                                <div className="card-body">
+                                    <h5 className="card-title">{stat.title}</h5>
+                                    <p className="card-text">
+                                        {stat.value}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+            <h3 className='p-3'>Quelques graphiques</h3>
+            <div className='row'>
+            </div>
+        </div>
+    )
+}
+
+export default Stats;
