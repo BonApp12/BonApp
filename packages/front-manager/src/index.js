e311b6224f63881a6a742f396585bee517f816d1
BOUABDELLAH Marwane <manbou92@hotmail.fr>
2022-02-26 01:36:17 +0100

    Import bootstrap librairies

--- packages/front-manager/src/index.js
+++ packages/front-manager/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.render(
     <App />,
