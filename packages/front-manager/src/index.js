be97e0625f20cb0aebef1a20fefabfd8905364c5
BOUABDELLAH Marwane <manbou92@hotmail.fr>
2022-02-26 02:24:35 +0100

    [FIX] CSS after the bootstrap core for the applying style

--- packages/front-manager/src/index.js
+++ packages/front-manager/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import App from './App';
 
 ReactDOM.render(
     <App />,
