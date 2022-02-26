578a6e088a1cd995c2440a8fb4f2578f497232ec
BOUABDELLAH Marwane <manbou92@hotmail.fr>
2022-02-26 02:23:45 +0100

    Import navbar component

--- packages/front-manager/src/App.js
+++ packages/front-manager/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-        <div className="container-fluid">
-            <div className="row flex-nowrap">
-                <Sidebar />
+    return (
+        <div className="App">
+            <div className="container-fluid">
+                <div className="row flex-nowrap">
+                    <Sidebar/>
+                    <Navbar/>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
