11254b896706abc32bb835d83efd7337ef9d45a9
BOUABDELLAH Marwane <manbou92@hotmail.fr>
2022-02-28 22:32:35 +0100

    [CARD] New structure of Card

--- packages/front-manager/src/components/Stats.js
+++ packages/front-manager/src/components/Stats.js
@@ -9,28 +9,36 @@ function Stats() {
         title: 'Plat le plus commandé',
         value: 'Grec'
     }, {
-        class: 'card border-3 border-start-0 border-end-0 border-warning mb-3', title: 'Commandes', value: Math.random()
-    }, {
-        class: 'card border-3 border-start-0 border-dark mb-3', title: 'Utilisateurs', value: Math.random()
+        class: 'card border-3 border-start-0 border-warning mb-3', title: 'Commandes', value: Math.random()
     }];
 
-    return (<div className='p-3'>
-            <h3 className='p-3'>Statistiques principales</h3>
+    return (
+        <div>
+            <div
+                className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 className="h2">Dashboard</h1>
+            </div>
             <div className="row">
                 {stats.map((stat) => {
-                    return (<div className="col-sm-3 text-center">
+                    return (
+                        <div className="col-sm-4 text-center">
                             <div className={stat.class}>
                                 <div className="card-body">
-                                    <h5 className="card-title">{stat.title}</h5>
+                                    <h5 className="card-title">
+                                        {stat.title}
+                                    </h5>
                                     <p className="card-text">
                                         {stat.value}
                                     </p>
                                 </div>
                             </div>
-                        </div>)
+                        </div>
+                    )
                 })}
             </div>
-        </div>)
+        </div>
+
+    )
 }
 
 export default Stats;
