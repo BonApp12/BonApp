721a279bfef382c3678e435abf5796d2a007f7ba
BOUABDELLAH Marwane <manbou92@hotmail.fr>
2022-02-28 19:47:24 +0100

    Refactor the return

--- packages/front-manager/src/App.js
+++ packages/front-manager/src/App.js
@@ -4,19 +4,17 @@ import Navbar from "./components/Navbar";
 import Stats from "./components/Stats";
 
 function App() {
-    return (
-        <div className="App">
-            <div className="container-fluid">
-                <div className="row flex-nowrap">
-                    <Sidebar/>
-                    <div className="col p-2">
-                        <Navbar/>
-                        <Stats/>
-                    </div>
+    return (<div className="App">
+        <div className="container-fluid">
+            <div className="row flex-nowrap">
+                <Sidebar/>
+                <div className="col p-2">
+                    <Navbar/>
+                    <Stats/>
                 </div>
             </div>
         </div>
-    );
+    </div>);
 }
 
 export default App;
