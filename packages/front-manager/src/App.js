e144851d73a351debc8a8666adf78f9efeb7878b
BOUABDELLAH Marwane <manbou92@hotmail.fr>
2022-02-28 22:42:02 +0100

    Adding the new graph component + changing structure of the app

--- packages/front-manager/src/App.js
+++ packages/front-manager/src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
-import Navbar from "./components/Navbar";
 import Stats from "./components/Stats";
-
+import Header from "./components/Header";
+import Graph from "./components/Graph";
 function App() {
-    return (<div className="App">
-        <div className="container-fluid">
-            <div className="row flex-nowrap">
-                <Sidebar/>
-                <div className="col p-2">
-                    <Navbar/>
-                    <Stats/>
+    return (
+        <div className="App">
+            <Header/>
+            <div className="container-fluid">
+                <div className="row">
+                    <Sidebar/>
+                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                        <Stats/>
+                        <Graph/>
+                    </main>
                 </div>
             </div>
         </div>
-    </div>);
+    );
 }
 
 export default App;
