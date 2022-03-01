3700a67d8fdb9fed17c44b9ded213f7d1124f1bf
BOUABDELLAH Marwane <manbou92@hotmail.fr>
2022-03-01 14:24:15 +0100

    [COMPONENT] Removing the Navbar component

--- packages/front-manager/src/App.js
+++ packages/front-manager/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
-import Navbar from "./components/Navbar";
 import Stats from "./components/Stats";
 
 function App() {
-    return (<div className="App">
-        <div className="container-fluid">
-            <div className="row flex-nowrap">
-                <Sidebar/>
-                <div className="col p-2">
-                    <Navbar/>
-                    <Stats/>
+    return (
+        <div className="App">
+            <div className="container-fluid">
+                <div className="row flex-nowrap">
+                    <Sidebar/>
+                    <div className="col p-2">
+                        <Stats/>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>);
+    );
 }
 
 export default App;
