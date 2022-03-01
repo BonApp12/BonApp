b93991fbccd8bb703543941ec831a737d8374e0a
BOUABDELLAH Marwane <manbou92@hotmail.fr>
2022-03-01 14:34:09 +0100

    [COMPONENT] Adding Graph component

--- packages/front-manager/src/App.js
+++ packages/front-manager/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import Stats from "./components/Stats";
 import Header from "./components/Header";
+import Graph from "./components/Graph";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                     <Sidebar/>
                     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                         <Stats/>
+                        <Graph/>
                     </main>
                 </div>
             </div>
---
+++ packages/front-manager/src/components/Graph.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Graph() {
+
+    return(<p>graph</p>)
+}
