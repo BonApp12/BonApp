5cf0875e372cdac7305e271fec5554fc3c301a87
BOUABDELLAH Marwane <manbou92@hotmail.fr>
2022-03-01 14:25:12 +0100

    [COMPONENT] Adding the Header component

--- packages/front-manager/src/App.js
+++ packages/front-manager/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Stats from "./components/Stats";
+import Header from "./components/Header";
 
 function App() {
     return (
