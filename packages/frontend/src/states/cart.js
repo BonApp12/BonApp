b0745e2086ee52e556973e995e04633f157190f5
Yassine Bousaidi <Yassine.bousaidi@hotmail.com>
2022-03-15 23:06:14 +0100

    * Update du docker * ajoute le systême de panier * ajoute les fixtures sur les plates * Suppression doublon bouton *remplacement de package-lock.json par yarn * update des plates

---
+++ packages/frontend/src/states/cart.js
@@ -0,0 +1,14 @@
+import {atom} from 'recoil';
+import {recoilPersist} from 'recoil-persist';
+
+const {persistAtom} = recoilPersist();
+
+//this is a store which will be used in whole in all application -> https://recoiljs.org/fr/
+const cartAtom = atom({
+    key: 'cart',
+    default: [],
+    //persist user in state. It's for reload the page because without that the state is remove
+    effects_UNSTABLE: [persistAtom]
+});
+
+export {cartAtom};
