bcb029a14931ea472888cb31760107e403d5e0e0
Marwane BOUABDELLAH <manbou92@hotmail.fr>
2022-03-16 23:55:56 +0100

    [NEST] Adding the order controller to the module

--- packages/backend/src/orders/orders.module.ts
+++ packages/backend/src/orders/orders.module.ts
@@ -3,9 +3,11 @@ import { OrdersService } from './orders.service';
 import { OrdersGateway } from './orders.gateway';
 import { Order } from './entities/order.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrdersController } from './orders.controller';
 
 @Module({
   providers: [OrdersGateway, OrdersService],
   imports: [TypeOrmModule.forFeature([Order])],
+  controllers: [OrdersController],
 })
 export class OrdersModule {}
