f34b5e1613eaa838a70a3ec605f0a483036202c9
Marwane BOUABDELLAH <manbou92@hotmail.fr>
2022-03-16 23:21:22 +0100

    Fetch table "plate" in DB

---
+++ packages/front-manager/src/requests/fetchFullPlate.js
@@ -0,0 +1,10 @@
+const fetchFullPlate= () => {
+    return fetch(process.env.REACT_APP_URL_BACKEND + '/plate', {
+        crossDomain: true,
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include',
+    })
+}
+
+export default fetchFullPlate;
