0c19b04d7052f5db0a15958f7daf448cc24d64e6
Marwane <manbou92@hotmail.fr>
2022-03-17 23:54:41 +0100

    remove response json

--- packages/front-manager/src/requests/updateOrder.js
+++ packages/front-manager/src/requests/updateOrder.js
@@ -1,10 +1,10 @@
-export default function updateOrder(id) {
+export default function updateOrder(idOrder) {
 
-    return fetch(process.env.REACT_APP_URL_BACKEND + '/orders/' + id, {
+    return fetch(process.env.REACT_APP_URL_BACKEND + '/orders/' + idOrder, {
         crossDomain: true,
         method: 'PATCH',
         mode: 'cors',
         credentials: 'include',
-    }).then((result) => {result.json()})
+    })
 
 }
