294efb514cb7d750e4a9c32521e5cd7202acc0ef
Marwane BOUABDELLAH <manbou92@hotmail.fr>
2022-03-24 22:53:06 +0100

    data

--- packages/front-manager/src/components/stats.js
+++ packages/front-manager/src/components/stats.js
@@ -2,8 +2,23 @@ import React, {useEffect, useState} from 'react'
 import fetchFullPlate from "../requests/fetchFullPlate";
 import fetchFullOrder from "../requests/fetchFullOrder";
 import updateOrder from "../requests/updateOrder";
+import moment from "moment";
+import 'moment/locale/fr';
 
 function Stats() {
+
+    let formattedDate, checkStatus;
+
+    checkStatus = (status) => {
+        if (status === 'to-do')
+            return true
+    }
+
+    formattedDate = (date) => {
+        moment.locale('fr')
+        return moment(date).calendar();
+    }
+
 
     const [plates, setPlate] = useState(0)
 
@@ -13,7 +28,7 @@ function Stats() {
             .then(plates => {
                 setPlate(plates)
             })
-    }, [])
+    }, [plates])
 
     const [orders, setOrders] = useState([])
 
@@ -23,16 +38,22 @@ function Stats() {
             .then(orders => {
                 setOrders(orders)
             })
-    }, [])
+    }, [orders])
+
+
 
     const stats = [{
-        class: 'card border-3 border-end-0 border-primary mb-3', title: 'Nombre de plats', value: plates.length
+        class: 'card border-3 border-end-0 border-primary mb-3',
+        title: 'Nombre de plats',
+        value: plates.length
     }, {
         class: 'card border-3 border-start-0 border-end-0 border-info mb-3',
         title: 'Plat le plus commandé',
         value: 'Grec'
     }, {
-        class: 'card border-3 border-start-0 border-warning mb-3', title: 'Commandes', value: orders.length
+        class: 'card border-3 border-start-0 border-warning mb-3',
+        title: 'Commandes',
+        value: orders.length
     }];
 
     return (
@@ -56,18 +77,23 @@ function Stats() {
                 </tr>
                 </thead>
                 <tbody>
+
                 {orders.map((order, index)  => (
                 <tr key={index}>
                     <th scope="row">{order.id}</th>
                     <td>{order.user.lastname}</td>
                     <td>{order.user.firstname}</td>
-                    <td>{order.created_at}</td>
+                    <td>{formattedDate(order.created_at)} </td>
                     <td>{order.plate.name}</td>
-                    <td>{order.status}</td>
+                    <td>{checkStatus(order.status) ? 'A faire' : 'Fini'}</td>
                     <td>
-                        <button className="btn btn-success" onClick={() => updateOrder(order.id)}>
-                            Valider
-                        </button>
+                        {checkStatus(order.status) ? (
+                            <button className="btn btn-success" onClick={() => updateOrder(order.id)}>
+                                Valider
+                            </button>
+                        ) : (
+                            'Aucune action'
+                        )}
                     </td>
                 </tr>
                 ))}
--- packages/front-manager/src/requests/fetchFullOrder.js
+++ packages/front-manager/src/requests/fetchFullOrder.js
@@ -1,5 +1,5 @@
 const fetchFullOrder = (idOrder) => {
-    return fetch(process.env.REACT_APP_URL_BACKEND + '/orders/restaurant/' + idOrder, {
+    return fetch(process.env.REACT_APP_URL_BACKEND + '/orders/restaurant/' + idOrder + '/status/to-do', {
         crossDomain: true,
         method: 'GET',
         mode: 'cors',
