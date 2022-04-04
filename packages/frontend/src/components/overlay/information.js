34d0945b3ba3f832ca762eba3f07f376ef063438
Yassine Bousaidi <Yassine.bousaidi@hotmail.com>
2022-04-04 05:35:49 +0200

    * Corrige la condition de rendu ProductsList.js * Corrige le rendu pour la modal

--- packages/frontend/src/components/overlay/information.js
+++ packages/frontend/src/components/overlay/information.js
@@ -5,14 +5,14 @@ import {AiOutlineCloseCircle} from "react-icons/ai";
 export function Information({displayModal, setDisplayModal, children}) {
     return (
         <div>
-            <div className={`Modal ${displayModal ? 'Show' : ''}`}>
+            <div className={`Modal ${displayModal?.isOpen ? 'Show' : ''}`}>
                 <button className="p-4 float-right text-2xl hover:text-rose-500 ease-in duration-300"
                         onClick={() => setDisplayModal(!displayModal)}>
                     <AiOutlineCloseCircle/>
                 </button>
                 {children}
             </div>
-            <div className={`Overlay ${displayModal ? 'Show' : ''}`} onClick={() => setDisplayModal(!displayModal)}/>
+            <div className={`Overlay ${displayModal?.isOpen ? 'Show' : ''}`} onClick={() => setDisplayModal(!displayModal)}/>
         </div>
 
     );
