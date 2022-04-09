061458544a46c2f92e93bac048c37fd3e802a393
Marwane <manbou92@hotmail.fr>
2022-04-09 02:43:54 +0200

    add params

--- packages/front-manager/src/requests/fetchFullOrder.js
+++ packages/front-manager/src/requests/fetchFullOrder.js
@@ -1,10 +1,19 @@
-const fetchFullOrder = (idOrder) => {
-    return fetch(process.env.REACT_APP_URL_BACKEND + '/orders/restaurant/' + idOrder + '/status/to-do', {
-        crossDomain: true,
-        method: 'GET',
-        mode: 'cors',
-        credentials: 'include',
-    })
+const fetchFullOrder = (idOrder, params) => {
+    if (params === 'only-orders') {
+        return fetch(process.env.REACT_APP_URL_BACKEND + '/orders/restaurant/' + idOrder, {
+            crossDomain: true,
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include',
+        })
+    } else if (params === 'to-do') {
+        return fetch(process.env.REACT_APP_URL_BACKEND + '/orders/restaurant/' + idOrder + '/status/to-do', {
+            crossDomain: true,
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include',
+        })
+    }
 }
 
 export default fetchFullOrder;
