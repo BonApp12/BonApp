1b663ec85a8d43b1f2944fe80a3eab8b8b9a7496
Marwane <manbou92@hotmail.fr>
2022-04-17 17:18:55 +0200

    tables entity

---
+++ packages/backend/src/tables/entities/tables.entity.ts
@@ -0,0 +1,18 @@
+import {BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn,} from 'typeorm';
+import {Order} from "../../orders/entities/order.entity";
+import {Restaurant} from "../../restaurant/entities/restaurant.entity";
+
+@Entity()
+export class Tables extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column('varchar', {length: 10})
+    libelle: string;
+
+    @ManyToOne(() => Order, (order: Order) => order.table)
+    order: Order;
+
+    @ManyToOne(() => Restaurant, (restaurant: Restaurant) => restaurant.table)
+    restaurant: Restaurant;
+}
