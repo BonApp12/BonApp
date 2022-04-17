69dc3682d0885dddcc7bc7715104c9369960637e
Marwane <manbou92@hotmail.fr>
2022-04-17 15:11:05 +0200

    Ratings entity

--- packages/backend/src/ratings/entities/ratings.entity.ts
+++ packages/backend/src/ratings/entities/ratings.entity.ts
@@ -1,13 +1,21 @@
-import {BaseEntity, Column, Entity, PrimaryGeneratedColumn,} from 'typeorm';
+import {BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn,} from 'typeorm';
+import {Users} from 'src/users/entities/users.entity';
+import {Plate} from "src/plate/entities/plate.entity";
 
 @Entity()
 export class Ratings extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column('varchar', {length: 200})
-    notation: string;
+    @Column()
+    notation: number;
 
     @Column('text')
     description: string;
+
+    @ManyToOne(() => Plate, (plate: Plate) => plate.orders)
+    plate: Plate;
+
+    @ManyToOne(() => Users, (user: Users) => user.ratings)
+    user: Users;
 }
