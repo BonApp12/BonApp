5d931e3632b82f789ae1b9b5270ad995c88440c1
Yassine Bousaidi <Yassine.bousaidi@hotmail.com>
2022-04-17 19:48:32 +0200

    * Corrige la PR de youcef

--- packages/backend/src/auth/auth.controller.ts
+++ packages/backend/src/auth/auth.controller.ts
@@ -1,85 +1,92 @@
 import {
-  Controller,
-  Post,
-  Get,
-  Req,
-  UseGuards,
-  Res,
-  Headers,
-  Body,
-  HttpCode,
-  Injectable, HttpException, HttpStatus,
+    Body,
+    Controller,
+    Get,
+    Headers,
+    HttpCode,
+    HttpException,
+    HttpStatus,
+    Injectable,
+    Post,
+    Req,
+    Res,
+    UseGuards,
 } from '@nestjs/common';
 import {Response} from 'express';
-import { LocalAuthGuard } from './local-auth.guard';
-import { AuthService } from './auth.service';
-import { JwtAuthGuard } from './jwt-auth.guard';
-import { CreateUsersDto } from '../users/dto/create-users.dto';
-import { UsersDto } from '../users/dto/users.dto';
+import {LocalAuthGuard} from './local-auth.guard';
+import {AuthService} from './auth.service';
+import {JwtAuthGuard} from './jwt-auth.guard';
+import {CreateUsersDto} from '../users/dto/create-users.dto';
+import {UsersDto} from '../users/dto/users.dto';
 import RequestWithUser from './interfaces/requestWithUser.interface';
-import { plainToClass } from 'class-transformer';
+import {plainToClass} from 'class-transformer';
+import {ConfigService} from "@nestjs/config";
+import {UserRole} from "../users/UserRole.enum";
 
 @Injectable()
 @Controller('auth')
 export class AuthController {
-  constructor(
-      private readonly authService: AuthService
-  ) {}
-
-  @UseGuards(JwtAuthGuard)
-  @Get()
-  authenticate(@Req() req: RequestWithUser) {
-    const user = req.user;
-    user.password = undefined;
-    return user;
-  }
-
-  @HttpCode(200)
-  @UseGuards(LocalAuthGuard)
-  @Post('/login')
-  //TODO: TEST:
-  // 1/ Test si tout fonctionnel -> a) Status 200 + token | b) UserDto
-  // 2/ mot de passe ou email incorrect -> test le retour
-  // 3/ mauvais objet (mauvais format) -> return 400
-  async login(@Req() req: RequestWithUser, @Headers() headers: any) {
-    const userDto = plainToClass(UsersDto,req.user);
-    if((headers.origin === process.env.URL_FRONTMANAGER && userDto.role !== 'CLIENT')||(headers.origin === process.env.URL_FRONTEND && userDto.role === 'CLIENT')) {
-      return this.loginUser(req,userDto);
+    constructor(
+        private readonly authService: AuthService,
+        private readonly configService: ConfigService
+    ) {
     }
-    throw new HttpException(
-        "Vous n'avez pas accès",
-        HttpStatus.UNAUTHORIZED,
-    );
-  }
 
-  private loginUser(req,user) {
-    req.res.setHeader('Set-Cookie', [
-      this.authService.getCookieWithJwtAccessToken(user.id),
-    ]);
-    return { statusCode: 200, user: user };
-  }
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    authenticate(@Req() req: RequestWithUser) {
+        const user = req.user;
+        user.password = undefined;
+        return user;
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Post('/isConnected')
-  async isConnected(@Req() req) {
-    return this.authService.isUserConnected(req.user);
-  }
+    @HttpCode(200)
+    @UseGuards(LocalAuthGuard)
+    @Post('/login')
+    //TODO: TEST:
+    // 1/ Test si tout fonctionnel -> a) Status 200 + token | b) UserDto
+    // 2/ mot de passe ou email incorrect -> test le retour
+    // 3/ mauvais objet (mauvais format) -> return 400
+    async login(@Req() req: RequestWithUser, @Headers() headers: any) {
+        const userDto = plainToClass(UsersDto, req.user);
+        if ((headers.origin === this.configService.get("URL_FRONTMANAGER") && userDto.role !== UserRole.CLIENT)
+            || (headers.origin === this.configService.get("URL_FRONTEND") && userDto.role === UserRole.CLIENT)) {
+            return this.loginUser(req, userDto);
+        }
+        throw new HttpException(
+            {type: "role", message: "Vous n'avez pas le droit d'accéder à cette ressource"},
+            HttpStatus.UNAUTHORIZED,
+        );
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('/profile')
-  getProfile(@Req() req) {
-    return this.authService.getCompleteUser(req.user);
-  }
+    private loginUser(req, user) {
+        req.res.setHeader('Set-Cookie', [
+            this.authService.getCookieWithJwtAccessToken(user.id),
+        ]);
+        return {statusCode: 200, user: user};
+    }
 
-  @Post('/logout')
-  async logout(@Req() req: RequestWithUser, @Res() res: Response) {
-    req.res.setHeader('Set-Cookie', this.authService.getCookiesForLogOut());
-    res.clearCookie('Authentication');
-    req.res.send();
-  }
+    @UseGuards(JwtAuthGuard)
+    @Post('/isConnected')
+    async isConnected(@Req() req) {
+        return this.authService.isUserConnected(req.user);
+    }
 
-  @Post('/register')
-  async register(@Body() registrationData: CreateUsersDto) {
-    return this.authService.register(registrationData);
-  }
+    @UseGuards(JwtAuthGuard)
+    @Get('/profile')
+    getProfile(@Req() req) {
+        return this.authService.getCompleteUser(req.user);
+    }
+
+    @Post('/logout')
+    async logout(@Req() req: RequestWithUser, @Res() res: Response) {
+        req.res.setHeader('Set-Cookie', this.authService.getCookiesForLogOut());
+        res.clearCookie('Authentication');
+        req.res.send();
+    }
+
+    @Post('/register')
+    async register(@Body() registrationData: CreateUsersDto) {
+        return this.authService.register(registrationData);
+    }
 }
--- packages/front-manager/src/views/auth/Login.js
+++ packages/front-manager/src/views/auth/Login.js
@@ -10,83 +10,88 @@ import login from "../../requests/login";
 import {toast} from "react-toastify";
 
 export default function Login() {
-  const [loading, setLoading] = useState(false);
-  const {register, handleSubmit, setError, formState: {errors}} = useForm({resolver: yupResolver(ValidationSchemaLogin())})
-  const history = useHistory();
-  const [userState, setUserState] = useRecoilState(userAtom);
+    const [loading, setLoading] = useState(false);
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: {errors}
+    } = useForm({resolver: yupResolver(ValidationSchemaLogin())});
+    const history = useHistory();
+    const [userState, setUserState] = useRecoilState(userAtom);
 
-  useEffect(() => {
-    userState !== '' && userState.role !== 'CLIENT' && history.push('/admin');
-  },[userState, history]);
+    useEffect(() => {
+        userState?.length && history.push('/admin');
+    }, [userState, history]);
 
-  const onSubmit = (user) => {
-    setLoading(true);
-    login(user)
-        .then(res => res.json())
-        .then(data => {
-          setLoading(false);
-          (data.statusCode === 400 || data.statusCode === 401) && setError('auth',{type: 'error', message: data.message});
-          data.statusCode === 200 && setUserState(data.user);
-          toast.success("Connexion réussi")
-          history.push('/admin');
-        })
-        .catch(err => {
-          setLoading(false);
-          console.log(err);
-          setError('auth',{type: 'error', message: err.response.data.message});
-        });
-  }
+    const onSubmit = (user) => {
+        setLoading(true);
+        login(user)
+            .then(res => res.json())
+            .then(responseLogin => {
+                setLoading(false);
+                if ([400, 401].includes(responseLogin.statusCode))
+                    return setError('auth', {type: 'error', message: responseLogin.message});
+                if (responseLogin.statusCode === 200) setUserState(responseLogin.user);
+                toast.success("Connexion réussi");
+                history.push('/admin');
+            })
+            .catch(err => {
+                setLoading(false);
+                setError('auth', {type: 'error', message: err.response.data.message});
+            });
+    };
 
-  return (
-    <>
-      <div className="container mx-auto px-4 h-full">
-        <div className="flex content-center items-center justify-center h-full">
-          <div className="w-full lg:w-4/12 px-4">
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200
+    return (
+        <>
+            <div className="container mx-auto px-4 h-full">
+                <div className="flex content-center items-center justify-center h-full">
+                    <div className="w-full lg:w-4/12 px-4">
+                        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200
             border-0">
-              <div className="flex-auto px-4 lg:px-10 pb-10">
-                {
-                    errors?.auth?.message && (
-                        <div className="mt-5 text-center">
-                          <span className="text-sm text-red-500">{errors?.auth?.message}*</span>
-                        </div>
-                    )
-                }
-                <form onSubmit={handleSubmit(onSubmit)}>
-                  <Input
-                    register={{...register('email')}}
-                    error={errors.email}
-                    label="Email"
-                    type="email"
-                    name="email"
-                    placeHolder="Votre adresse email"
-                  />
+                            <div className="flex-auto px-4 lg:px-10 pb-10">
+                                {
+                                    errors?.auth?.message && (
+                                        <div className="mt-5 text-center">
+                                            <span className="text-sm text-red-500">{errors?.auth?.message}*</span>
+                                        </div>
+                                    )
+                                }
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <Input
+                                        register={{...register('email')}}
+                                        error={errors.email}
+                                        label="Email"
+                                        type="email"
+                                        name="email"
+                                        placeHolder="Votre adresse email"
+                                    />
 
-                  <Input
-                    register={{...register('password')}}
-                    error={errors.password}
-                    label="Mot de passe"
-                    type="password"
-                    name="password"
-                    placeHolder="Votre mot de passe"
-                  />
+                                    <Input
+                                        register={{...register('password')}}
+                                        error={errors.password}
+                                        label="Mot de passe"
+                                        type="password"
+                                        name="password"
+                                        placeHolder="Votre mot de passe"
+                                    />
 
-                  <div className="text-center mt-6">
-                    <button
-                      className={`btn ${loading && 'loading'} bg-orange-300 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3
+                                    <div className="text-center mt-6">
+                                        <button
+                                            className={`btn ${loading && 'loading'} bg-orange-300 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3
                       rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear
                       transition-all duration-150`}
-                      type="submit"
-                    >
-                      {loading ? 'En cours...' : 'Se connecter'}
-                    </button>
-                  </div>
-                </form>
-              </div>
+                                            type="submit"
+                                        >
+                                            {loading ? 'En cours...' : 'Se connecter'}
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
