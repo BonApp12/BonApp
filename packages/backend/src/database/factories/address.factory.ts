97666ccc97ac58e8c1f141aad58f5bc3d753cda1
Marwane <manbou92@hotmail.fr>
2022-04-17 17:19:33 +0200

    rename field address to street

--- packages/backend/src/database/factories/address.factory.ts
+++ packages/backend/src/database/factories/address.factory.ts
@@ -5,7 +5,7 @@ import {Address} from '../../address/entities/address.entity';
 
 define(Address, (faker: typeof Faker) => {
     const address = new Address();
-    address.address = faker.address.streetAddress();
+    address.street = faker.address.streetAddress();
     address.city = faker.address.city();
     address.postal_code = faker.address.zipCode('#####');
     return address;
