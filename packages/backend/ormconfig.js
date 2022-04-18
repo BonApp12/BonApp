9218a9c5de891483ef44f397790341ecd44b5f96
Marwane <manbou92@hotmail.fr>
2022-04-18 12:55:00 +0200

    reset to false

--- packages/backend/ormconfig.js
+++ packages/backend/ormconfig.js
@@ -6,7 +6,7 @@ module.exports = {
   password: 'root',
   database: 'bp-pg-db',
   entities: ['dist/**/*.entity{.ts,.js}'],
-  synchronize: true, // Changer en true quand une entité est modifiée pour que les changements s'appliquent. docker logs -f backend pour vérifier les erreurs de démarrage s'il y a lieu
+  synchronize: false, // Changer en true quand une entité est modifiée pour que les changements s'appliquent. docker logs -f backend pour vérifier les erreurs de démarrage s'il y a lieu
   logging: true,
   migrationsTableName: 'custom_migration_table',
   migrations: ['dist/migration/*{.ts,.js}'],
