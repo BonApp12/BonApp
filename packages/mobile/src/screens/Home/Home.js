566fe1e76dc689c7e4eca108119dcd3eb806271a
Yassine Bousaidi <yassine.bousaidi@hotmail.com>
2022-06-22 01:00:40 +0200

    * Ajoute le swipe gesture

--- packages/mobile/src/screens/Home/Home.js
+++ packages/mobile/src/screens/Home/Home.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import {View} from 'react-native';
-import {Button, Layout} from '@ui-kitten/components';
+import React, {useState} from 'react';
+import {ScrollView, StyleSheet, Text} from 'react-native';
+import SafeAreaView from "react-native/Libraries/Components/SafeAreaView/SafeAreaView";
+import ListItem from "../../components/listItem";
+
+// MOCK LES ORDERS
+const TITLES = [
+    'Record the dismissible tutorial 🎥',
+    'Leave 👍🏼 to the video',
+    'Check YouTube comments',
+    'Subscribe to the channel 🚀',
+    'Leave a ⭐️ on the GitHub Repo',
+];
+const TASKS = TITLES.map((title, index) => ({index, title}));
 
 export const Home = () => {
+    const [tasks, setTasks] = useState(TASKS);
     return (
-        <View style={{flex: 1, backgroundColor: '#000000'}}>
-            <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Button onPress={() => alert('let\'s gooooo')}>OPEN DETAILS</Button>
-            </Layout>
-        </View>
+        <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+            <Text style={styles.title}>Commande prête</Text>
+            <ScrollView>
+                {tasks.map(task =>
+                    <ListItem task={task}
+                              onDismiss={(id) => {
+                                  setTasks(tasks.filter(task => task.index !== id));
+                              }}
+                              key={task.index}
+                    />)}
+            </ScrollView>
+        </SafeAreaView>
     );
 };
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 40,
+        marginVertical: 20,
+        paddingLeft: '5%',
+    }
+});
