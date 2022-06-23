583e1fe87eb58c81614104c1d38243cfa64e3c0d
Yassine Bousaidi <yassine.bousaidi@hotmail.com>
2022-06-23 02:31:49 +0200

    * Ajoute les notifications

--- packages/mobile/App.js
+++ packages/mobile/App.js
@@ -1,14 +1,26 @@
 import {AuthProvider} from './src/context/AuthContext';
 import Main from "./Main";
 import Toast from 'react-native-toast-message';
+import * as Notifications from 'expo-notifications';
+import NotificationProvider from "./NotificationProvider";
 
-export default function App(){
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+});
+
+
+export default function App() {
     return (
         <>
             <AuthProvider>
-                <Main />
+                <Main/>
             </AuthProvider>
-            <Toast />
+            <NotificationProvider/>
+            <Toast/>
         </>
-    )
+    );
 }
---
+++ packages/mobile/NotificationProvider.js
@@ -0,0 +1,30 @@
+import * as Notifications from 'expo-notifications';
+import React, {useEffect, useRef} from 'react';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+    }),
+});
+export default function NotificationProvider() {
+    const notificationListener = useRef();
+    const responseListener = useRef();
+
+    useEffect(() => {
+
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            console.log(notification);
+        });
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener();
+
+        return () => {
+            Notifications.removeNotificationSubscription(notificationListener.current);
+            Notifications.removeNotificationSubscription(responseListener.current);
+        };
+    }, []);
+
+    return null;
+}
