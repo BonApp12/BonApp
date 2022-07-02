7842dcd993bc1ae17c6b42f870dcab9b1cf0a19c
Marwane <manbou92@hotmail.fr>
2022-07-02 03:19:06 +0200

    adding url serveur OVH

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,5 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
@@ -7,7 +7,11 @@ async function bootstrap() {
   app.enableCors({
     credentials: true,
     //TODO: mettre les url dans un .env
-    origin: ['http://localhost:3000', "http://localhost:3001", "http://localhost:8888","http://localhost:8081","http://localhost:8080"],
+    origin: [
+      'http://localhost:3000', "http://localhost:3001", "http://localhost:8888", "http://localhost:8081",
+      "http://localhost:8080", "http://51.68.45.99:3000/", "http://51.68.45.99:3001/", "http://51.68.45.99:8888/",
+      "http://51.68.45.99:8081/", "http://51.68.45.99:8080/"
+    ],
   });
   app.use(cookieParser());
   await app.listen(4000);
