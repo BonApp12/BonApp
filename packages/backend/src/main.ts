0d35cf7e048d2ac808a7ec2043c2759f4b8c51b5
Marwane <manbou92@hotmail.fr>
2022-07-02 19:56:27 +0200

    test

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: '*',
-    origin: ['http://51.68.45.99:3000'],
+    origin: ['http://51.68.45.99:3000', 'http://51.68.45.99:3001', 'http://51.68.45.99:8080', 'http://51.68.45.99:8888', 'http://51.68.45.99:8081'],
   });
   app.use(cookieParser());
   await app.listen(4000);
