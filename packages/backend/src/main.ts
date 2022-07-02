6d3237ec258da48f7734ed6f1fdd4853dfc9c24e
Marwane <manbou92@hotmail.fr>
2022-07-02 03:53:50 +0200

    no-cors test

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -5,13 +5,9 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    credentials: true,
+    //credentials: true,
     //TODO: mettre les url dans un .env
-    origin: [
-      'http://localhost:3000', "http://localhost:3001", "http://localhost:8888", "http://localhost:8081",
-      "http://localhost:8080", "http://51.68.45.99:3000/", "http://51.68.45.99:3001/", "http://51.68.45.99:8888/",
-      "http://51.68.45.99:8081/", "http://51.68.45.99:8080/"
-    ],
+    origin: '*',
   });
   app.use(cookieParser());
   await app.listen(4000);
