38ea6c2ff0153d72605189c2e8da621fd17965ac
Marwane <manbou92@hotmail.fr>
2022-07-02 19:13:27 +0200

    no-cors test

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: '*',
-    origin: '*',
+    origin: ['http://51.68.45.99:3000'],
   });
   app.use(cookieParser());
   await app.listen(4000);
