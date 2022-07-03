42e97fd8357e90e86fc3034b5fc0ef122c8789f1
Marwane <manbou92@hotmail.fr>
2022-07-03 17:59:53 +0200

    adding urls

--- packages/front-manager/src/requests/login.js
+++ packages/front-manager/src/requests/login.js
@@ -1,6 +1,6 @@
 const login = (user) => {
     console.log(process.env.REACT_APP_URL_BACKEND);
-    return fetch(process.env.REACT_APP_URL_BACKEND + '/auth/login', {
+    return fetch(process.env.REACT_APP_URL_BACKEND + 'auth/login', {
         crossDomain: true,
         method: 'POST',
         mode: 'cors',
