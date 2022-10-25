69c5cda84dc50606e2edacb3fc7ed8c34e324836
12Wass <wassimdah@gmail.com>
2022-10-25 04:01:35 +0200

    filtering: adding a new tab for Orders : splitting ongoing orders and terminated orders

--- packages/front-manager/src/components/Cards/CompletedOrders.js
+++ packages/front-manager/src/components/Cards/CompletedOrders.js
@@ -3,46 +3,39 @@ import * as dayjs from 'dayjs';
 import 'dayjs/locale/fr';
 
 import fetchFullOrder from "../../requests/fetchFullOrder";
-import updateOrder from "../../requests/updateOrder";
-import resetUserConnected from "../../helpers/resetUserConnected";
 import {useRecoilState} from "recoil";
 import {userAtom} from "../../states/user";
-import {useHistory} from "react-router-dom";
-import Modal from "../Modal/Modal";
-import {SocketContext} from "../../contexts/socket";
-import {toast} from "react-toastify";
 import OrderStatusEnum from "../Enum/OrderStatusEnum";
+import resetUserConnected from "../../helpers/resetUserConnected";
+import Modal from "../Modal/Modal";
+import {useHistory} from "react-router-dom";
 
-export default function CardTable() {
+
+
+export default function CompletedOrders(){
 
     const [orders, setOrders] = useState([]);
     const [modalInfo, setModalInfo] = useState(null);
     const [orderReceived, setOrderReceived] = useState(false);
 
+    const [userState, setUserState] = useRecoilState(userAtom);   
+    const history = useHistory();
+
     let checkStatus, formattedDate;
 
-    const [userState, setUserState] = useRecoilState(userAtom);
-    const history = useHistory();
-    const socket = useContext(SocketContext);
-
     checkStatus = (status) => {
-        return status === OrderStatusEnum.TODO || status === OrderStatusEnum.READY;
+        return status === OrderStatusEnum.COMPLETED;
     };
 
     formattedDate = (date) => {
         dayjs.locale('fr');
         return dayjs(date).format('DD/MM/YYYY à HH:mm');
-    };
+    }
 
-    useEffect(() => {
-        socket.on('orderCreated', () => {
-            setOrderReceived(true);
-            toast.success('Une nouvelle commande a été créée !');
-        });
-    }, []);
 
+    
     useEffect(() => {
-        fetchFullOrder(userState?.restaurant.id, [OrderStatusEnum.TODO, OrderStatusEnum.READY]).then(res => {
+        fetchFullOrder(userState?.restaurant.id, OrderStatusEnum.COMPLETED).then(res => {
             if (res.status === 401) return resetUserConnected(setUserState, history);
             return res.json();
         }).then(resOrder => {
@@ -52,21 +45,8 @@ export default function CardTable() {
         });
         return function cleanup() {
             setOrders([]);
-        };
+        }
     }, [orderReceived]);
-
-    useEffect(() => {
-        socket.emit('StaffJoinRestaurantRoom', {user: userState});
-    }, [socket]);
-
-    const updateOrderStatus = (idOrder, status) => {
-        status === OrderStatusEnum.TODO ? status = OrderStatusEnum.READY : status = OrderStatusEnum.COMPLETED;
-        updateOrder(idOrder, status).then((res) => res.json())
-            .then((result) => {
-                socket.emit('updateOrder', {order: result.raw[0]});
-                setOrderReceived(true);
-            });
-    };
 
     function computeTotal(orderPlates) {
         let total = 0;
@@ -76,6 +56,7 @@ export default function CardTable() {
         return total;
     }
 
+
     return (
         <>
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white">
@@ -83,7 +64,7 @@ export default function CardTable() {
                     <div className="flex flex-wrap items-center">
                         <div className="relative w-full px-4 max-w-full flex-grow flex-1">
                             <h3 className="font-semibold text-lg light text-blueGray-700">
-                                Commandes en cours
+                                Commandes terminées
                             </h3>
                         </div>
                     </div>
@@ -187,11 +168,7 @@ export default function CardTable() {
                                                :
                                                'fas fa-circle text-green-500 mr-2'}
                                     />
-                                    {order.status === OrderStatusEnum.TODO ?
-                                        'En cours'
-                                        :
-                                        'Prête !'
-                                    }
+                                    Délivrée
                                 </td>
                                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                                     <p>
@@ -200,15 +177,7 @@ export default function CardTable() {
                                     </p>
                                 </td>
                                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                    {checkStatus(order.status) ? (
-                                        <button
-                                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
-                                            onClick={() => updateOrderStatus(order.id, order.status)}>
-                                            Valider
-                                        </button>
-                                    ) : (
-                                        'Aucune action'
-                                    )}
+                                    Aucune action
                                 </td>
                             </tr>
                         ))}
@@ -225,7 +194,7 @@ export default function CardTable() {
                                 return (
                                     <div key={plate.id} className="mt-3">
                                         <p>{plate.plate.name}
-                                            <b>x{plate.quantity}</b> - <b>{plate.price * plate.quantity} €</b></p>
+                                            <b> x {plate.quantity}</b> - <b>{plate.price * plate.quantity} €</b></p>
                                     </div>
                                 );
                             })
--- packages/front-manager/src/requests/fetchFullOrder.js
+++ packages/front-manager/src/requests/fetchFullOrder.js
@@ -1,7 +1,8 @@
 const fetchFullOrder = (restaurantId, params) => {
     let url = process.env.REACT_APP_URL_BACKEND + 'orders/restaurant/' + restaurantId;
     if (params === 'to-do') url = process.env.REACT_APP_URL_BACKEND + `orders/restaurant/${restaurantId}/status/to-do`;
-
+    if (params === 'completed') url = process.env.REACT_APP_URL_BACKEND + `orders/restaurant/${restaurantId}/status/completed`;
+    if (params.includes('to-do') && params.includes('ready')) url = process.env.REACT_APP_URL_BACKEND + `orders/restaurant/${restaurantId}/status/not-completed`;
     return fetch(url, {
         crossDomain: true,
         method: 'GET',
